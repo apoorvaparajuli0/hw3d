247c003340b05b02896e73f680cb2b014f5254ec
Apoorva Parajuli <apoorvaparajuli0@gmail.com>
2023-06-30 01:57:17 -0700

    added comment templates

--- hw3d/Keyboard.cpp
+++ hw3d/Keyboard.cpp
@@ -9,6 +9,9 @@ bool Keyboard::KeyIsPressed(unsigned char keycode) const noexcept
 	return keystates[keycode];
 }
 
+/*
+*
+*/
 Keyboard::Event Keyboard::ReadKey() noexcept
 {
 	if (keybuffer.size() > 0u)
@@ -24,11 +27,17 @@ Keyboard::Event Keyboard::ReadKey() noexcept
 	}
 }
 
+/*
+*
+*/
 bool Keyboard::KeyIsEmpty() const noexcept
 {
 	return keybuffer.empty();
 }
 
+/*
+*
+*/
 char Keyboard::ReadChar() noexcept
 {
 	if (charbuffer.size() > 0u)
@@ -44,42 +53,66 @@ char Keyboard::ReadChar() noexcept
 	}
 }
 
+/*
+*
+*/
 bool Keyboard::CharIsEmpty() const noexcept
 {
 	return charbuffer.empty();
 }
 
+/*
+*
+*/
 void Keyboard::FlushKey() noexcept
 {
 	keybuffer = std::queue<Event>();
 }
 
+/*
+*
+*/
 void Keyboard::FlushChar() noexcept
 {
 	charbuffer = std::queue<char>();
 }
 
+/*
+*
+*/
 void Keyboard::Flush() noexcept
 {
 	FlushKey();
 	FlushChar();
 }
 
+/*
+*
+*/
 void Keyboard::EnableAutorepeat() noexcept
 {
 	autorepeatEnabled = true;
 }
 
+/*
+*
+*/
 void Keyboard::DisableAutorepeat() noexcept
 {
 	autorepeatEnabled = false;
 }
 
+/*
+*
+*/
 bool Keyboard::AutorepeatIsEnabled() const noexcept
 {
 	return autorepeatEnabled;
 }
 
+/*
+*
+*/
 void Keyboard::OnKeyPressed(unsigned char keycode) noexcept
 {
 	keystates[keycode] = true;
@@ -87,6 +120,9 @@ void Keyboard::OnKeyPressed(unsigned char keycode) noexcept
 	TrimBuffer(keybuffer);
 }
 
+/*
+*
+*/
 void Keyboard::OnKeyReleased(unsigned char keycode) noexcept
 {
 	keystates[keycode] = false;
@@ -94,17 +130,26 @@ void Keyboard::OnKeyReleased(unsigned char keycode) noexcept
 	TrimBuffer(keybuffer);
 }
 
+/*
+*
+*/
 void Keyboard::OnChar(char character) noexcept
 {
 	charbuffer.push(character);
 	TrimBuffer(charbuffer);
 } 
 
+/*
+*
+*/
 void Keyboard::ClearState() noexcept
 {
 	keystates.reset();
 }
 
+/*
+*
+*/
 template<typename T>
 void Keyboard::TrimBuffer(std::queue<T>& buffer) noexcept
 {
